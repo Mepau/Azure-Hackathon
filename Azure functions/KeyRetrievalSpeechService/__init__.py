ca71fae0d6058afa58ceef57342079ebf5e91d22
Michael Perez <michaelperezabreu@gmail.com>
2022-05-02 20:23:31 -0400

    Implemented azure function KeyRetrievalSpeechService

--- Azure functions/KeyRetrievalSpeechService/__init__.py
+++ Azure functions/KeyRetrievalSpeechService/__init__.py
@@ -1,24 +1,38 @@
 import logging
-
 import azure.functions as func
-
+import urllib.request
+import json
+import os
+import ssl
+import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    #new_req_body = str.encode(json.dumps(words))
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    #Define with the susbcription key for the Azure Speech Service
+    speechKey = ""
+    #Define with the region of said Azure Speech Service
+    speechRegion = ""
+
+    headers = { "Ocp-Apim-Subscription-Key": speechKey, "Content-Type": "application/json"}
+
+    url = f"https://{speechRegion}.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+
+    new_req = urllib.request.Request(url, {} ,headers)
+
+    try:
+        
+        response = urllib.request.urlopen(new_req)
+
+        result = response.read()
+        
+        logging.info(f"words {result}")
+
+        return func.HttpResponse(f"{result}")
+    except urllib.error.HTTPError as error:
+        logging.info("The request failed with status code: " + str(error.code))
+
+       # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        logging.info(error.info())
+        logging.info(error.read().decode("utf8", 'ignore'))
